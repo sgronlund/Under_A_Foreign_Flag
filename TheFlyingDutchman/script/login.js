89c92aacd151912eb2e812b2ab55af8e74a23498
Fredrik Engstrand <frewacom@gmail.com>
2021-02-16 17:59:14 +0100

    Added customer page markup

--- TheFlyingDutchman/script/login.js
+++ TheFlyingDutchman/script/login.js
@@ -1,3 +1,8 @@
+function save_user_details(details) {
+    // Save the user details so that they can be used on the customer pages
+    localStorage.setItem('user_details', JSON.stringify(details));
+}
+
 // Shows or hides the login menu
 function show_login(arg) {
     if (arg === 'show') {
@@ -9,29 +14,31 @@ function show_login(arg) {
 
 // Simple login function
 function login() {
-  let user_in = $('#username').val(); // Get username from input in html
-  let password_in = $('#password').val(); // Get password from input in html
-  let details = userDetails(user_in); // Get user details from DB, if username does not exist in DB will return nothing
+    let user_in = $('#username').val(); // Get username from input in html
+    let password_in = $('#password').val(); // Get password from input in html
+    let details = userDetails(user_in); // Get user details from DB, if username does not exist in DB will return nothing
 
-  if (details.password === password_in) { // Compares password
-    login_error('hide');
-    // Checks credentials and redirects to the right side
-    if (details.credentials == 0) {
-      window.location.href = 'vipcustom.html'; // Redirect
-    } else if (details.credentials > 0) {
-      window.location.href = 'staff.html'; // Redirect
+    if (details.password === password_in) { // Compares password
+        login_error('hide');
+
+        // Checks credentials and redirects to the right side
+        if (details.credentials == 0) {
+            save_user_details(details);
+            window.location.href = 'customer.html'; // Redirect
+        } else if (details.credentials > 0) {
+            window.location.href = 'staff.html'; // Redirect
+        }
+    } else {
+        login_error('show');
     }
-  } else {
-    login_error('show');
-  }
 }
 
 function login_error(arg) {
-  if (arg == 'show') {
-    $('#username').addClass('login_error');
-    $('#password').addClass('login_error');
-  } else {
-    $('#username').removeClass('login_error');
-    $('#password').removeClass('login_error');
-  }
+    if (arg == 'show') {
+        $('#username').addClass('login_error');
+        $('#password').addClass('login_error');
+    } else {
+        $('#username').removeClass('login_error');
+        $('#password').removeClass('login_error');
+    }
 }
