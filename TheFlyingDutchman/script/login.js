fd0cd2d82753a4df79174c8a2d1f20ac527209cf
dan-cmyk <dante.grenholm@gmail.com>
2021-02-16 10:40:06 +0100

    added login error

--- TheFlyingDutchman/script/login.js
+++ TheFlyingDutchman/script/login.js
@@ -1,20 +1,32 @@
-
-
 function show_login(arg) {
-    if (arg === "show") {
-        $(".login").css("display", "flex");
-    } else {
-        $(".login").css("display", "none");
-    }
+  if (arg === 'show') {
+    $('.login').css('display', 'flex');
+  } else {
+    $('.login').css('display', 'none');
+    login_error('hide');
+  }
 }
 
 // Simple login function
-function login(){
-    let user_in = $('#username').val(); // Get username from input in html
-    let password_in = $('#password').val(); // Get password from input in html
-    let details = userDetails(user_in); // Get user details from DB, if username does not exist in DB will return nothing
+function login() {
+  let user_in = $('#username').val(); // Get username from input in html
+  let password_in = $('#password').val(); // Get password from input in html
+  let details = userDetails(user_in); // Get user details from DB, if username does not exist in DB will return nothing
 
-    if(details.password === password_in){ // Compares password
-        window.location.href = "vipcustom.html"; // Redirect
-    }
+  if (details.password === password_in) { // Compares password
+    login_error('hide');
+    window.location.href = 'vipcustom.html'; // Redirect
+  } else {
+    login_error('show');
+  }
+}
+
+function login_error(arg) {
+  if (arg == 'show') {
+    $('#username').addClass('login_error');
+    $('#password').addClass('login_error');
+  } else {
+    $('#username').removeClass('login_error');
+    $('#password').removeClass('login_error');
+  }
 }
