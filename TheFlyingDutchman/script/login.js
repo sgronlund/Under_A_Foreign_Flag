7fe0aca5fd6538cf1f8a748592dcc0392e10d66d
dan-cmyk <dante.grenholm@gmail.com>
2021-02-16 17:36:18 +0100

    fixed

--- TheFlyingDutchman/script/login.js
+++ TheFlyingDutchman/script/login.js
@@ -1,6 +1,6 @@
 // Shows or hides the login menu
 function show_login(arg) {
-    if (arg === "show") {
+    if (arg === 'show') {
         $("#login-form").css("display", "flex"); // jQuery function for updating CSS declaration of a specific element based on the rule and the value
     } else {
         $("#login-form").css("display", "none"); // jQuery function for updating CSS declaration of a specific element based on the rule and the value
