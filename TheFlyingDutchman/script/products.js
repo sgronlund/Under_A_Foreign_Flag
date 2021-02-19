6dd804143b96e1e2894c519d33f778f0d557040e
Fredrik Engstrand <fredrik@engstrand.nu>
2021-02-19 14:12:19 +0000

    Added sticky customer header

--- TheFlyingDutchman/script/products.js
+++ TheFlyingDutchman/script/products.js
@@ -24,7 +24,7 @@ function render_products(id) {
 
 $(document).ready(function () {
     $('#db-script').on('load', () => {
-        console.log('asda');
-        // products = allBeverages();
+        products = allBeverages();
+        $(document).trigger('db-loaded');
     });
 });
