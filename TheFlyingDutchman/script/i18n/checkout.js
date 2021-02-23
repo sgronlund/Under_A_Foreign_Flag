1c56d699fee0a67177268f7fac98cc51b933eae3
Fredrik Engstrand <frewacom@gmail.com>
2021-02-23 12:51:50 +0100

    Added checkout modal buttons

--- TheFlyingDutchman/script/i18n/checkout.js
+++ TheFlyingDutchman/script/i18n/checkout.js
@@ -1,6 +1,9 @@
 add_localization_data('checkout', {
     keys: [
         'checkout_modal_title',
+        'checkout_pay_at_table_label',
+        'checkout_pay_at_bar_label',
+        'checkout_pay_with_credit_label',
     ],
     alt_keys: [],
     placeholder_keys: [],
@@ -14,11 +17,17 @@ add_localization_data('checkout', {
     en: {
         default: {
             checkout_modal_title: 'Checkout',
+            checkout_pay_at_table_label: 'Pay at table',
+            checkout_pay_at_bar_label: 'Pay at bar',
+            checkout_pay_with_credit_label: 'Pay with credit',
         },
     },
     sv : {
         default: {
             checkout_modal_title: 'Kassa',
+            checkout_pay_at_table_label: 'Betala vid bordet',
+            checkout_pay_at_bar_label: 'Betala vid baren',
+            checkout_pay_with_credit_label: 'Betala med saldo',
         },
     }
 });
